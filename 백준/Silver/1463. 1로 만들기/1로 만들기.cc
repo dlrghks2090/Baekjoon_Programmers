24c13fe34a3b359de666c7b7324f5cf645fa7ff9
dlrghks2090 <52392858+dlrghks2090@users.noreply.github.com>
2022-08-04 14:10:42 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,14 +1,9 @@
-// 백준 1463번_DP_실버3_1로 만들기 문제
-
-// arr 을 main()함수안의 지역변수로 선언했을때 메모리의 제한때문에 계속 오류가 났었다.
-// 메모리의 사용이 많은경우 data영역에 자료를 저장하는 전역변수나 정적변수(static)을 사용하자.
 #include<iostream>
-#include<algorithm>
 
 using namespace std;
 
 int N;
-int arr[1000001];
+int sol[1000001] = { 0, };
 
 int main() {
 
@@ -16,18 +11,26 @@ int main() {
 
 	cin >> N;
 
-	for (int i = 2; i <= N; i++) {
-		arr[i] = arr[i - 1] + 1;	// -1한 경우
-		if (i % 2 == 0) {
-			arr[i] = min(arr[i], arr[i/2]+1);		// -1한 경우와 2로 나눈경우 중 최솟값
+	sol[2] = 1;
+	sol[3] = 1;
+
+	for (int i = 4; i <= N; i++) {
+		int tmp = i;
+		int count = 0;
+
+		sol[i] = sol[i - 1] + 1;
+
+		if (tmp % 3 == 0) {
+			if (sol[tmp / 3] + 1 < sol[i])
+				sol[i] = sol[tmp / 3] + 1;
 		}
-		if (i % 3 == 0) {
-			arr[i] = min(arr[i], arr[i / 3] + 1);		// (-1한 경우 or 2로 나눈 경우)와 3으로 나눈경우 중 최솟값
+		if (tmp % 2 == 0) {
+			if (sol[tmp / 2] + 1 < sol[i])
+				sol[i] = sol[tmp / 2] + 1;
 		}
 	}
 
-	cout << arr[N];
-
+	cout << sol[N];
 
 	return 0;
 }
