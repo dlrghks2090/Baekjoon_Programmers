f013232a4c5fe8926c01e7835c96aeaa7630a550
dlrghks2090 <52392858+dlrghks2090@users.noreply.github.com>
2022-08-12 21:35:04 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.cc
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.cc
@@ -1,43 +1,33 @@
-// 백준_DP_16194번_실버1_카드 구매하기2 문제
-
 #include<iostream>
-#include<algorithm>
-#include<cstring>
 
 using namespace std;
-
-int dp[10001];
 
 int main() {
 
 	int N;
-	int* arr;
+	int arr[1001] = { 0, };
+	int dp[1001] = { 0, };
+
 
 	cin >> N;
-	arr = new int[N + 1];
-
-	arr[0] = 10000;
 
 	for (int i = 1; i <= N; i++) {
 		cin >> arr[i];
-	}	
+	}
 
-	memset(dp, 10000, sizeof(dp));		// for문보다 더 빠르다.
-
-	dp[0] = 0;
 	dp[1] = arr[1];
 
 	for (int i = 2; i <= N; i++) {
+		dp[i] = arr[i];
 		for (int j = 1; j <= i; j++) {
-			dp[i] = min(dp[i - j] + arr[j], dp[i]);
+			if (arr[i - j] + dp[j] < dp[i]) {
+				dp[i] = arr[i - j] + dp[j];
+			}
 		}
 	}
 
 	cout << dp[N];
 
-
-
-	delete[] arr;
 
 	return 0;
 }
