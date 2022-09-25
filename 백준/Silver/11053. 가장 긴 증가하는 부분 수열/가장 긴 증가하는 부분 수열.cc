ebfb2a7c4f1a95aa1c2653c29cee4d66c0e8880a
dlrghks2090 <52392858+dlrghks2090@users.noreply.github.com>
2022-09-25 22:10:25 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,47 +1,39 @@
 #include<iostream>
-#include<algorithm>
 
 using namespace std;
-
-int dp[1001];
 
 int main() {
 
 	int N;
-	int* arr;
+
+	int arr[1001] = { 0, };
+	int dp[1001] = { 0, };
 	int sol = 1;
 
 	cin >> N;
 
-	arr = new int[N+1];
+	fill_n(dp, 1001, 1);
+
+	//cout << dp[1000] << endl;
 
 	for (int i = 1; i <= N; i++) {
 		cin >> arr[i];
 	}
 
-
-	//dp의 모든 원소값을 1로 초기화
-	fill(dp, dp + 1001, 1);
-
 	for (int i = 2; i <= N; i++) {
 		for (int j = 1; j < i; j++) {
-			if (arr[i] > arr[j]) {
-
-				dp[i] = max(dp[i], dp[j] + 1);
-
+			if (arr[j] < arr[i]) {
+				if (dp[j] + 1 > dp[i])
+					dp[i] = dp[j] + 1;
 			}
 		}
-		sol = max(sol, dp[i]);
+		if (sol < dp[i]) {
+			sol = dp[i];
+		}
+	}
 
-	} 
-
-	//cout << dp[1] << " " << dp[2] << " " << dp[4] << " " << dp[6] << '\n';
 	cout << sol;
 
-
-
-
-	delete[] arr;
 
 	return 0;
 }
