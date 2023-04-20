4b3f6848c6055c1dcddc5f146190f4aecb846421
dlrghks2090 <52392858+dlrghks2090@users.noreply.github.com>
2023-04-20 16:47:17 +0900

    [level 3] Title: 네트워크, Time: 0.06 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int visit[201] = {0,};
 
-void bfs(vector<vector<int>> computers, int n, int idx) {
+void bfs(vector<vector<int>> computers, int n, int idx) {   //한번 실행되면 네트워크 1개 추가
     queue<int> que;
     
     que.push(idx);
@@ -18,8 +18,8 @@ void bfs(vector<vector<int>> computers, int n, int idx) {
         
         que.pop();
         
-        for (int i = 0; i < n; i++) {
-            if (computers[tmp][i] == 1 && visit[i] == 0) {
+        for (int i = 0; i < n; i++) {       // tmp번호의 컴퓨터와 연결된 컴퓨터 전부 확인
+            if (computers[tmp][i] == 1 && visit[i] == 0) {  // 연결된 컴퓨터 중에 방문하지 않았던 컴퓨터가 존재한다면 push
                 visit[i] = 1;
                 que.push(i);
             }
@@ -32,7 +32,7 @@ int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
     
     for(int i = 0; i<n; i++) {
-        if (visit[i] == 0) {
+        if (visit[i] == 0) {    // 방문되지 않는 컴퓨터일때 탐색 시작
             bfs(computers, n , i);
             answer++;
         }
