3fbb8a9fb70eceae68729d869b00235a7192a629
IkHwan Kim <52392858+dlrghks2090@users.noreply.github.com>
2023-05-12 11:30:08 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -1,28 +1,34 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 
 using namespace std;
 
-int N;
-int sol = 0;
-int arr[1001] = { 0, };
-
-
 int main() {
+
+	int N;
+	int arr[1001] = { 0, };
+	int num;
+	vector<int> v;
+	int sol = 0;
 
 	cin >> N;
 
 	for (int i = 1; i <= N; i++) {
-		cin >> arr[i];
+		cin >> num;
+		v.push_back(num);
 	}
 
-	sort(arr + 1, arr + 1 + N);
+	sort(v.begin(), v.end());
 
-	for (int i = 1; i <= N; i++) {
-		sol += arr[i] * (N + 1 - i);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j <= i; j++) {
+			sol += v[j];
+		}
 	}
 
 	cout << sol;
+
 
 	return 0;
 }
