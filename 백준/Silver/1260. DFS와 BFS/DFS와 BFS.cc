4b0852bdf8363189b2175e2943394b428e6cbd0a
IkHwan Kim <52392858+dlrghks2090@users.noreply.github.com>
2023-06-27 11:14:16 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 5976 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,81 +1,78 @@
 #include<iostream>
+#include<vector>
 #include<queue>
-#include<stack>
 
 using namespace std;
 
-int map[1001][1001];
-int visit[1001];
-int N;
-queue<int> q;
-stack<int> st;
+int arr[1001][1001] = { 0, };
+int visit[1001] = { 0, };
+int sol[10001] = { 0, };
+int flag = 0;
+int N, M, V;
+int a, b;
 
 
+void init_visit(int n) {
+
+	for (int i = 1; i <= n; i++) {
+		visit[i] = 0;
+	}
+}
 
 
+void dfs(int idx) {
+	cout << idx << " ";
+	visit[idx] = 1;
 
-
-void dfs(int v) {
-	
-	cout << v << " ";
-	visit[v] = 1;
 	for (int i = 1; i <= N; i++) {
-		if (map[v][i] == 1 && visit[i] == 0) {
+		if (visit[i] == 0 && arr[idx][i] == 1) {
 			dfs(i);
 		}
 	}
-
-
-
 }
 
-void bfs(int start) {
-	
-	cout << start << " ";
-	q.push(start);
+void bfs(int idx, int n) {
+	queue<int> que;
 
-	while (!q.empty()) {
-		int node = q.front();
-		q.pop();
-		for (int i = 1; i <= N; i++) {
-			if (map[node][i] == 1 && visit[i] == 0) {
-				visit[node] = 1;
-				visit[i]=1;
-				cout << i << " ";
-				q.push(i);
+	que.push(idx);
+	visit[idx] = 1;
+
+	while (!que.empty()) {
+		int tmp = que.front();
+		que.pop();
+		cout << tmp << " ";
+
+		for (int i = 1; i <= n; i++) {
+			if (arr[tmp][i] == 1 && visit[i] == 0) {
+				que.push(i);
+				visit[i] =1;
 			}
 		}
-		//cout << '\n';
-
 	}
-
-
-
 }
-
 
 int main() {
 
-	int M, V;
-	
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+
 
 	cin >> N >> M >> V;
 
-	for (int i = 0; i < M; i++) {
-		int a, b;
-		cin >> a >> b;
-		map[a][b] = map[b][a] = 1;
-	}
+	
 
+	for (int i = 0; i < M; i++) {
+		cin >> a >> b;
+		arr[a][b] = arr[b][a] = 1;
+	}
 
 	dfs(V);
 	cout << '\n';
-	//cout << "---------------" << '\n';
-	fill_n(visit, N + 1, 0);
-
-	bfs(V);
-
-
+	init_visit(N);
+	bfs(V, N);
+	
 
 
 	return 0;
