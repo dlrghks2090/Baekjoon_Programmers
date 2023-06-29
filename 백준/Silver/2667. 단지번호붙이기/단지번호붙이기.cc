5d93b528f0c1654bf61b0a798620b404f5b747fd
IkHwan Kim <52392858+dlrghks2090@users.noreply.github.com>
2023-06-29 10:42:04 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,114 +1,77 @@
 #include<iostream>
-#include<string>
 #include<vector>
-#include<algorithm>
+#include<string>
 #include<queue>
+#include<algorithm>
 
 using namespace std;
 
-int N;
-string str[26];
-int arr[27][27] = { 0, };
+int map[27][27] = { 0, };
 int visit[27][27] = { 0, };
-int total=0;
-vector<int> vec;
 
+int bfs(int N, int a, int b) {
+	queue<pair<int, int>> que;
+	pair<int, int> p;
+	int idx1[4] = { 0,1,0,-1 };
+	int idx2[4] = { 1,0,-1,0 };
+	int sol = 0;
 
-void bfs(int x, int y) {
-
-	queue<pair<int,int>> que;
-	int count = 0;
-	pair<int, int> p1;
-
-	p1.first = x;
-	p1.second = y;
-	que.push(p1);
-	visit[x][y] = 1;
-	count++;
-
-	while (1) {
-		int x1, y1;
-
-		if (que.empty() == true) {
-			vec.push_back(count);
-			break;
-		}
-
-		x1 = que.front().first;
-		y1 = que.front().second;
+	p.first = a;
+	p.second = b;
+	que.push(p);
+	sol++;
+	visit[a][b] = 1;
+	while (!que.empty()) {
+		int a2 = que.front().first;
+		int b2 = que.front().second;
 		que.pop();
 
-		if (arr[x1 + 1][y1] == 1 && visit[x1 + 1][y1] == 0) {
-			p1.first = x1 + 1;
-			p1.second = y1;
-			que.push(p1);
-			visit[x1 + 1][y1] = 1;
-			count++;
-		}
-		if (arr[x1 - 1][y1] == 1 && visit[x1 - 1][y1] == 0) {
-			p1.first = x1 - 1;
-			p1.second = y1;
-			que.push(p1);
-			visit[x1 - 1][y1] = 1;
-			count++;
-		}
-		if (arr[x1][y1 + 1] == 1 && visit[x1][y1 + 1] == 0) {
-			p1.first = x1;
-			p1.second = y1 + 1;
-			que.push(p1);
-			visit[x1][y1 + 1] = 1;
-			count++;
-		}
-		if (arr[x1][y1 - 1] == 1 && visit[x1][y1 - 1] == 0) {
-			p1.first = x1;
-			p1.second = y1 - 1;
-			que.push(p1);
-			visit[x1][y1 - 1] = 1;
-			count++;
+		
+		for (int i = 0; i < 4; i++) {
+			if (map[a2 + idx1[i]][b2 + idx2[i]] == 1 && visit[a2 + idx1[i]][b2 + idx2[i]] == 0) {
+				p.first = a2 + idx1[i];
+				p.second = b2 + idx2[i];
+				que.push(p);
+				sol++;
+				visit[p.first][p.second] = 1;
+			}
 		}
 
 	}
-
-
+	return sol;
 }
 
 int main() {
 
+	vector<int> num;
+	int N;
+	string str;
+	int count = 0;
+
 	cin >> N;
 
-	for (int i = 0; i < N; i++) {
-		cin >> str[i];
-	}
-
 	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			arr[i][j] = str[i - 1][j - 1] - '0';
+		cin >> str;
+		for (int j = 0; j < str.size(); j++) {
+			map[i][j + 1] = str[j] - '0';
 		}
 	}
 
-	/*cout << "arr 확인!!!" << endl;
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			cout << arr[i][j];
-		}
-		cout << endl;
-	}*/
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
-			if (visit[i][j] == 0 && arr[i][j] == 1) {
-				total++;
-				bfs(i, j);
+			if (visit[i][j] == 0 && map[i][j] == 1) {
+				num.push_back(bfs(N, i, j));
+				count++;
 			}
 		}
 	}
 
-	sort(vec.begin(), vec.end());
+	sort(num.begin(), num.end());
 
-	cout << total << endl;
-
-	for (int i = 0; i < vec.size(); i++) {
-		cout << vec[i] << endl;
+	cout << count << '\n';
+	for (int i = 0; i < num.size(); i++) {
+		cout << num[i] << '\n';
 	}
 
 	return 0;
