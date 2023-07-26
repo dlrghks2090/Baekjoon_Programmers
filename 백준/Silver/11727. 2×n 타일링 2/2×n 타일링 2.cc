136eee1cc4a961180505064452dd6bc3f408f534
IkHwan Kim <52392858+dlrghks2090@users.noreply.github.com>
2023-07-26 18:06:35 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -1,24 +1,43 @@
+// 백준_실버3_DP_2*n타일링2
+/*
+풀이>
+문제에서 주어졌다시피, 타일의 끝은 총 3가지 경우만 올 수 있다.
+	1. ~~~|
+	2. ~~~~=
+	3. ~~~~ㅁ 
+
+=> 따라서 dp[n]을 구할때도 이 세 경우의 수를 구해 더한값이 2*n타일의  모든 경우를 센것이 된다.
+
+=> dp[n] = dp[n-1] + dp[n-2] + dp[n-2];
+*/
+
+
 #include<iostream>
 
-using namespace std;
 
-int arr[1001];
+using namespace std;
 
 int main() {
 
 	int n;
-	
+	int* dp;
+
 	cin >> n;
 
-	arr[1] = 1;
-	arr[2] = 3;
+	dp = new int[n + 1]{ 0, };
 
-	for (int i = 3; i <= n; i++)
-	{
-		arr[i] = (arr[i - 1] + arr[i - 2]*2)%10007;
+	
+
+	dp[1] = 1;
+	dp[2] = 3;
+	
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 2])%10007;
 	}
+	
+	cout << dp[n];
 
-	cout << arr[n];
+	delete[] dp;
 
 	return 0;
 }
