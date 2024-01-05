c285ca003735a89055056d765dc49b69921d153a
IkHwan Kim <52392858+dlrghks2090@users.noreply.github.com>
2024-01-05 20:21:34 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 2;
+    
+    while(1) {
+        if (n%answer == 1) {
+            break;
+        }
+        answer++;
+    }
+    return answer;
+}
