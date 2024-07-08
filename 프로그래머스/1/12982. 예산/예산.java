b13b64fc6f5e9998140926a0a3bef59ce4615f32
IkHwan Kim <52392858+dlrghks2090@users.noreply.github.com>
2024-07-08 17:31:18 +0900

    [level 1] Title: 예산, Time: 0.39 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Arrays.sort(d);
+        
+        for(int i=0; i<d.length; i++) {
+            if (d[i] > budget) {
+                break;
+            }
+            else{
+                budget -= d[i];
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
