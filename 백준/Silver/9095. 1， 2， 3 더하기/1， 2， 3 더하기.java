f1bb11b5f8e66af3da125f49c7be19c7689a1111
IkHwan Kim <52392858+dlrghks2090@users.noreply.github.com>
2025-03-10 19:03:10 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 116 ms, Memory: 15788 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,41 +1,36 @@
 import java.io.*;
 
-
-class Main {
-    public static void main(String[] args) throws IOException {
+public class Main {
+    public static int sol=0;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
 
-        int arr[] = new int[11];
+        for (int i=0; i<T; i++) {
+            sol = 0;
+            int N = Integer.parseInt(br.readLine());
+            rec(N, 0);
 
-        /*
-            arr[1] = 1;
-            arr[2] = 2;
-            arr[3] = 4;
-            arr[4] = 7;
-            arr[5] = 13;
-            arr[6] = 24;
-            arr[7] = 44;
-        */
-
-        // arr[n] = arr[n-1] + arr[n-2] + arr[n-3];
-
-        arr[1] = 1;
-        arr[2] = 2;
-        arr[3] = 4;
-
-        for (int i = 4; i <= 10; i++) {
-            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+            bw.write(sol+"\n");
+            bw.flush();
         }
 
-        for (int i = 0; i < T; i++) {
-            int n = Integer.parseInt(br.readLine());
-            bw.write(arr[n] + "\n");
-        }
-
-        bw.flush();
         bw.close();
+
+    }
+
+    public static void rec(int n, int total) {
+        if (total == n) {
+            sol++;
+            return;
+        }
+
+        for (int i=1; i<=3; i++) {
+            if (total+i <= n) {
+                rec(n, total+i);
+            }
+        }
     }
 }
